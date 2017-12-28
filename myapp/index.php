7e56552c095d537ab8d9fbf66c666a751e6682fb
msatyanp <maharjan.satya@gmail.com>
2017-12-28 14:58:00 +0545

    first commit

---
+++ myapp/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Docker php </title>
+    </head>
+    <body>
+        <p>
+            Hello. from docker with php  Today is <?= date('l \t\h\e jS') ?>.
+        </p>
+    </body>
+</html>
